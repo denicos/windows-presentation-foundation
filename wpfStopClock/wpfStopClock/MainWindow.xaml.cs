12c0cce5a25664df940509e898a1694c717601f1
Denicos <nicoswaves.mn@gmail.com>
2019-06-13 20:34:26 +0300

    stop clock
    ..

---
+++ wpfStopClock/wpfStopClock/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfStopClock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+
+    {
+        wpfStopClock.Ticker _localTicker;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _localTicker = (wpfStopClock.Ticker)this.FindResource("localTicker");
+        }
+
+        public void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            _localTicker.Start();
+        }
+        private void stopButton_Click(object sender, RoutedEventArgs e)
+        {
+            _localTicker.Stop();
+        }
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            _localTicker.Clear();
+        }
+    }
+}
---
+++ wpfStopClock/wpfStopClock/Ticker.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace wpfStopClock
+{
+    /// <summary>
+    /// This class is a simple time ticker class.
+    /// </summary>
+  public class Ticker : FrameworkElement
+
+    {
+      private TimeSpan currentInterval;
+      private DispatcherTimer tmr;
+      private DateTime startTime;
+
+      public static readonly DependencyProperty DisplayIntervalProperty =
+          DependencyProperty.Register("DisplayInterval", typeof(string), typeof(Ticker), null);
+
+
+      public string DisplayInterval
+      {
+          get { return (string)GetValue(DisplayIntervalProperty); }
+          set { SetValue(DisplayIntervalProperty, value); }
+      }
+     
+
+      public Ticker()
+      {
+          tmr = new DispatcherTimer();
+          tmr.Interval = new TimeSpan(0, 0, 0, 0, 100);
+          tmr.Tick += new EventHandler(tmr_Tick);
+          UpdateValues(true);
+      }
+      public void Start()
+      {
+          if (!tmr.IsEnabled)
+          {
+              startTime = DateTime.Now - currentInterval;
+              tmr.Start();
+          }
+      }
+      public void Stop()
+      {
+          if (tmr.IsEnabled)
+          {
+              tmr.Stop();
+          }
+      }
+      public void Clear()
+      {
+          startTime = DateTime.Now;
+          UpdateValues(true);
+      }
+      private void tmr_Tick(object sender, EventArgs e)
+      {
+          UpdateValues(false);
+      }
+
+      private void UpdateValues(bool reset)
+      {
+          if (reset)
+              currentInterval = new TimeSpan();
+          else
+              currentInterval = DateTime.Now - startTime;
+          DisplayInterval = string.Format("{0:D2} : {1:D2}", currentInterval.Minutes, currentInterval.Seconds);
+      }
+
+    }
+}
